1e63e72e90650773ff2dbabc94329ab3a0c98075
Eirik Vullum <evullum@gmail.com>
2015-02-19 00:48:57 +0100

    cleanup

--- client.jsx
+++ client.jsx
@@ -6,8 +6,8 @@ var Router = require('react-router');
 var routes = require('./routes.jsx');
 
 document.addEventListener('DOMContentLoaded', function(event) {
-    Router.run(routes, Router.HistoryLocation, function(Handler, state) {
-      var serverState = window.__serverState || {};
-      React.render(<Handler __serverState={serverState} />, document.body);
-    });
+  Router.run(routes, Router.HistoryLocation, function(Handler, state) {
+    var serverState = window.__serverState || {};
+    React.render(<Handler __serverState={serverState} />, document.body);
+  });
 });
