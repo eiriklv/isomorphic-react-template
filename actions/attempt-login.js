f8c98128c76024cfccf15c38756d83612430b429
Eirik Vullum <evullum@gmail.com>
2015-02-28 18:22:37 +0100

    cleanup

--- actions/attempt-login.js
+++ actions/attempt-login.js
@@ -6,9 +6,6 @@ module.exports = function(context, payload, done) {
   context.Api.logIn(payload, function(err, result) {
     if (err) return done(err);
 
-    console.log('-------------------------');
-    console.log(result);
-
     if (!result.user) {
       context.Dispatcher.emit('SET_USER_AS_LOGGED_OUT');
     } else {
