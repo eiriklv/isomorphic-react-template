d54e53333e79660c6519ba6e587250df3ecb8f71
Eirik Vullum <evullum@gmail.com>
2015-03-02 01:13:45 +0100

    factor the session-api out of server-api

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const config = require('./config');
 const appSetup = require('./app-init-setup');
 const app = appSetup.init();
 
+app.use('/api', require('./session-api'));
 app.use('/api', require('./server-api'));
 app.use('/', require('./flux/server.jsx'));
 
--- server-api/index.js
+++ server-api/index.js
@@ -3,11 +3,7 @@
 var express = require('express');
 var router = express();
 
-require('./auth');
-
 router.use(require('./routes/places-single')('/places/:id'));
 router.use(require('./routes/places-all')('/places'));
-router.use(require('./routes/session')('/session'));
-router.use(require('./routes/signup')('/signup'));
 
 module.exports = router;
